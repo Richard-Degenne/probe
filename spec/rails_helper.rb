27b95af27de9de4f03dfc0815ab15ebd10305921
Richard Degenne <richard.degenne+github@gmail.com>
2022-05-20 12:21:14 +0200

    Added Rubocop

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -49,7 +51,7 @@ RSpec.configure do |config|
   # You can disable this behaviour by removing the line below, and instead
   # explicitly tag your specs with their type, e.g.:
   #
-  #     RSpec.describe UsersController, type: :controller do
+  #     describe UsersController, type: :controller do
   #       # ...
   #     end
   #
